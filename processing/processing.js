7649d59d176e8fb93c8fca00c775d070e7213399
Łukasz Starosta <lstarosta98@gmail.com>
2019-06-05 13:12:45 +0200

    Design

--- processing/processing.js
+++ processing/processing.js
@@ -3,5 +3,7 @@ const cameraData = {
     front: JSON.parse(sessionStorage.getItem('inputFileFrontCamera'))
 };
 
-document.getElementById('col1').innerText += sessionStorage.getItem('inputFileTopCamera');
-document.getElementById('col2').innerText += sessionStorage.getItem('inputFileFrontCamera');
+function clearSessionStorage() {
+    sessionStorage.removeItem('inputFileTopCamera');
+    sessionStorage.removeItem('inputFileFrontCamera');
+}
